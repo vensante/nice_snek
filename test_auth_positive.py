c7ce7011b88324c559451c6743678f1951a06baa
vensante <45139147+vensante@users.noreply.github.com>
2021-10-08 19:51:02 +0300

    Add files via upload

---
+++ test_auth_positive.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+link = "C:/edu/test-protei/qa-test.html" # replace the file path with a valid one
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_id("loginEmail")
+    input1.send_keys("test@test.ru")
+    input2 = browser.find_element_by_id("loginPassword")
+    input2.send_keys("test")
+    time.sleep(1)
+    button = browser.find_element_by_id("authButton")
+    button.click()
+    authSuccess = browser.find_element_by_id("inputsPage")
+    assert authSuccess != 0, 'OK'
+
+finally:
+    time.sleep(1)
+    browser.quit()
